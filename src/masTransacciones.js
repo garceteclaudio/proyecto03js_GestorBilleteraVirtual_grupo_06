7654ec6bd47e192dbfdf9841609849b00d5f2af4
garceteclaudio <garcete.claudio@gmail.com>
2024-09-19 22:52:38 -0300

    fix mas transacciones

--- src/masTransacciones.js
+++ src/masTransacciones.js
@@ -3,9 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const cuentas = JSON.parse(localStorage.getItem("cuentas")) || [];
 
-  cuentas.sort((a, b) => b.transacciones - a.transacciones);
+  const cuentasAgrupadas = {};
 
   cuentas.forEach((cuenta) => {
+    const key = `${cuenta.nombre}-${cuenta.billetera}`;
+
+    if (cuentasAgrupadas[key]) {
+      cuentasAgrupadas[key].transacciones += cuenta.transacciones;
+    } else {
+      cuentasAgrupadas[key] = {
+        nombre: cuenta.nombre,
+        billetera: cuenta.billetera,
+        transacciones: cuenta.transacciones,
+      };
+    }
+  });
+
+  const cuentasUnicas = Object.values(cuentasAgrupadas);
+
+  cuentasUnicas.sort((a, b) => b.transacciones - a.transacciones);
+
+  cuentasUnicas.forEach((cuenta) => {
     const item = document.createElement("div");
     item.textContent = `${cuenta.nombre} - ${cuenta.billetera} - ${cuenta.transacciones} transacciones`;
     listado.appendChild(item);
